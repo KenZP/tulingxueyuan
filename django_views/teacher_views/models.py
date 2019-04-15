0a84a3d061d9fd3d804cda8f214b952c18009fe4
KenZP <398984762@qq.com>
2019-04-15 15:50:25 +0800

    update django models db study 0415

--- django_views/teacher_views/models.py
+++ django_views/teacher_views/models.py
@@ -7,4 +7,10 @@ class Teacher(models.Model):
 	name = models.CharField(max_length=12)
 	age = models.IntegerField()
 	address = models.CharField(max_length=50)
-	course = models.CharField(max_length=20)	
+	course = models.CharField(max_length=20)
+
+
+	def __str__(self):
+		return self.name
+
+			
